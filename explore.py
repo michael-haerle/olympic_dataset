ecdd10c8d633b26ffdaba362cf3532c1d6ca9707
Michael Haerle <michael.haerle01@gmail.com>
2023-03-03 08:43:57 -0600

    added a explore.py file

---
+++ explore.py
@@ -0,0 +1,34 @@
+# Standard imports
+import pandas as pd
+
+# Visulation Imports
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# import splitting and imputing functions
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+
+# Modeling imports
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from sklearn.feature_selection import f_regression 
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_selection import SelectKBest, f_regression, RFE
+import sklearn.preprocessing
+import statsmodels.api as sm
+from sklearn.cluster import DBSCAN
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+
+# turn off pink boxes
+import warnings
+warnings.filterwarnings("ignore")
